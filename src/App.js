2a46d6dc1593e52caadf0f3c43adf3b67b4e3dac
Hamza Msutafa <hamzamustafa37@icloud.com>
2020-06-07 19:09:39 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      this is main div rha
+  
+    </div>
+  );
+}
+
+export default App;
